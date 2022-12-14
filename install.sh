e8cf7e58ba9d17db9a7f84e6cd175489eb5bdfcb
shotastage <hornet.live.mf@gmail.com>
2022-12-14 10:56:20 +0900

    add compiler

--- install.sh
+++ install.sh
@@ -2,9 +2,18 @@
 
 # Install Homebrew
 
-if [ -e /Application/Xcode.app ]; then
+if [ -e /Applications/Xcode.app ]; then
     echo "Xcode is installed"
 else
     echo "Xcode is not installed"
     exit 1
 fi
+
+cd
+mkdir .codk_setup
+cd .codk_setup
+git clone https://github.com/shotastage/CoreOverlay.git
+cd CoreOverlay
+swift build
+cd
+rm -rf .codk_setup
